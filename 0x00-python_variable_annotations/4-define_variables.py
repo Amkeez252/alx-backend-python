b5ab917a8122aaa93d87fa576865675d4a9e262e
Amkeez252 <auwalamalik@gmail.com>
2023-07-06 20:47:28 +0000

    Define variables

--- 0x00-python_variable_annotations/4-define_variables.py
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
-'''Task 4 Define variables
-'''
+"""Define variables."""
 
 
 a: int = 1
 pi: float = 3.14
 i_understand_annotations: bool = True
-school: str = 'Holberton'
+school: str = "Holberton"
