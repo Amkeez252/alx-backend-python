c2e75f570f28e293f33f0a58e1e7bbb517557822
Amkeez252 <auwalamalik@gmail.com>
2023-07-11 14:05:31 +0000

    Async Generator

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -11,7 +11,7 @@ async def async_generator() -> Generator[int, None, None]:
     Async Generator.
 
     Yields:
-        Generator: yield random number after waiting for 1 second
+        Generator: yield random number after waiting for 1 second.
     """
     for _ in range(10):
         await asyncio.sleep(1)
